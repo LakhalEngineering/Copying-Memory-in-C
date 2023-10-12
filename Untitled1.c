fc6971658c8f5f39db298f22cc5c7d6d1eeb585e
LakhalEngineering <125914042+LakhalEngineering@users.noreply.github.com>
2023-10-12 12:15:49 +0200

    using pointer
    Point this pointer (*n2) at location in to were we copying contents

--- Untitled1.c
+++ Untitled1.c
@@ -1,6 +1,9 @@
 #include <Stdio.h>
 #include <string.h> // to use memcpy fun
 
+#define NODE_OFFSET 10
+#define BUFFER_SIZE 4096
+
 typedef struct node{
     unsigned int isvalid : 1;
     unsigned int size : 15;
@@ -9,11 +12,14 @@ typedef struct node{
 
 int main(){
     node_t n = {.isvalid = 1, .size = 367, .next = NULL};
-    node_t n2;
+    node_t *n2;
 
-    memcpy(&n2, &n, sizeof(node_t)); // P1 : distination P2 : Adress of source P3 : size
+    char buffer[BUFFER_SIZE];
+
+    memcpy(buffer+NODE_OFFSET, &n, sizeof(node_t)); // P1 : distination P2 : Adress of source P3 : size
+    n2 = (node_t*)buffer+NODE_OFFSET;
 
     /* n2 = n; */
 
-    printf("n2 = %d, %d, %p\n",n2.isvalid, n2.size, n2.next);
+    printf("n2 = %d, %d, %p\n",n2->isvalid, n2->size, n2->next);
 }
